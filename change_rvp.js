3c0830d3e156bbc730eadbb53daf003dcdaa362f
vinishmakaroonie <138175501+vinishmakaroonie@users.noreply.github.com>
2024-01-20 16:34:16 +0800

    [S1-TECHNICAL] Module 1 & 2

---
+++ change_rvp.js
@@ -0,0 +1,12 @@
+function changeColors() {
+    const body = document.body;
+
+    // Set background color to midnight blue
+    const bgColor = '#3D0303';
+
+    // Set text color to off-white
+    const textColor = '#151B54';
+
+    body.style.backgroundColor = bgColor;
+    body.style.color = textColor;
+}
